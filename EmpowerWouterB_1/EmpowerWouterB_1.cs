132cb207255371100c9a0c353b91f488a441ff0e
Wouter Bogaert <wouter.bogaert@skyline.be>
2023-03-28 16:37:23 +0200

    order pizza

--- EmpowerWouterB_1/EmpowerWouterB_1.cs
+++ EmpowerWouterB_1/EmpowerWouterB_1.cs
@@ -55,6 +55,7 @@ namespace EmpowerWouterB_1
 	using System.Collections.Generic;
 	using System.Globalization;
 	using System.Text;
+	using Empower.Library.Room3;
 	using Skyline.DataMiner.Automation;
 	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
 
@@ -69,12 +70,16 @@ namespace EmpowerWouterB_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			var myDms = engine.GetDms();
-			var elements = myDms.GetElements();
-			foreach (var element in elements)
-			{
-				engine.GenerateInformation(element.Name);
-			}
+			//var myDms = engine.GetDms();
+			//var elements = myDms.GetElements();
+			//foreach (var element in elements)
+			//{
+			//	engine.GenerateInformation(element.Name);
+			//}
+
+			var order = OrderFactory.CreateOrder("Pizza Pepperoni");
+			order.Dispatch("WouterB");
+
 		}
 	}
 }
